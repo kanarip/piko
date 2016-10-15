a248e62cde599cae4a1f29190508582919270419
Jeroen van Meeuwen <kanarip@kanarip.ch>
2016-10-15 13:50:16 +0200

    Allow apps to define their own schemas

--- piko/db/__init__.py
+++ piko/db/__init__.py
@@ -18,6 +18,38 @@ sqlalchemy_i18n.make_translatable(db.Mapper)
 
 try:
     from .model import *
-except:
-    pass
+except Exception, errmsg:
+    app.logger.error("An exception occurred: %r" % (errmsg))
+    import traceback
+    app.logger.error(traceback.format_exc())
+
+#: The base path to search for additional applications.
+base_path = os.path.abspath(
+        os.path.join(
+                os.path.dirname(__file__),
+                '..',
+                'apps'
+            )
+    )
+
+for candidate in os.listdir(base_path):
+    # Must be a directory.
+    if not os.path.isdir(os.path.join(base_path, candidate)):
+        continue
+
+    mod_name = 'piko.apps.' + candidate + '.db'
+
+    try:
+        # Obtain the register function...
+        __import__(mod_name, fromlist = [ '*' ])
+
+    except ImportError, errmsg:
+        import traceback
+        app.logger.error("ImportError: %r" % (errmsg))
+        app.logger.error("%s" % (traceback.format_exc()))
+
+    except AttributeError, errmsg:
+        import traceback
+        app.logger.error("AttributeError: %r" % (errmsg))
+        app.logger.error("%s" % (traceback.format_exc()))
 
