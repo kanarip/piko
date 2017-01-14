75851fcf657e262b56bbc30a1dc6142f2e59a92b
Jeroen van Meeuwen <kanarip@kanarip.ch>
2017-01-14 12:21:05 +0100

    A proof-of-concept implementation of an end-point for Cyrus SASL authentication daemon account credentials verification.

--- piko/apps/cas/__init__.py
+++ piko/apps/cas/__init__.py
@@ -1,18 +1,31 @@
+"""
+    Central Authentication Services for piko.
+"""
 import os
 
 from flask import jsonify
+from flask import request
 
 from piko import App
 
+# pylint: disable=invalid-name
 template_path = os.path.abspath(
-        os.path.join(
-                os.path.dirname(__file__),
-                'templates'
-            )
+    os.path.join(
+        os.path.dirname(__file__),
+        'templates'
     )
+)
 
 def register(apps):
-    app = App('piko.cas', template_folder = template_path)
+    """
+        Register CAS as a Flask application.
+
+        :param apps:    A list of :py:class:`piko.App` applications.
+        :type  apps:    list
+        :returns:       A list of :py:class:`piko.App` applications with this
+                        application included.
+    """
+    app = App('piko.cas', template_folder=template_path)
     app.debug = True
     register_routes(app)
 
@@ -27,37 +40,90 @@ def register(apps):
     return apps
 
 def register_blueprint(app):
+    """
+        Register CAS as a Flask blueprint.
+    """
     from piko import Blueprint
 
     blueprint = Blueprint(
-            app,
-            'piko.cas',
-            __name__,
-            url_prefix='/cas',
-            template_folder=template_path
-        )
+        app,
+        'piko.cas',
+        __name__,
+        url_prefix='/cas',
+        template_folder=template_path
+    )
 
     register_routes(blueprint)
 
     app.register_blueprint(blueprint)
 
     blueprint = Blueprint(
-            app,
-            'piko.api.v1.cas',
-            __name__,
-            url_prefix='/api/v1/cas'
-        )
+        app,
+        'piko.api.v1.cas',
+        __name__,
+        url_prefix='/api/v1/cas'
+    )
 
     register_api_routes(blueprint)
 
     app.register_blueprint(blueprint)
 
 def register_routes(app):
-    pass
+    """
+        Register routes for the UI.
+    """
+    @app.route('/')
+    def dummy_index():
+        """
+            Dummy index
+        """
+        return app.abort(404)
 
 def register_api_routes(app):
-    @app.route('/', methods = [ 'POST' ])
-    def index():
-        return jsonify({
-                'result': True
-            })
+    """
+        Register routes for the API v1.
+    """
+    @app.route('/saslauthd', methods=['POST'])
+    # pylint: disable=unused-variable
+    def saslauthd():
+        """
+            Authentication end-point for a Cyrus SASL Authentication daemon.
+        """
+        from piko.db import db
+        from piko.db.model import Account
+
+        try:
+            username = request.form.get('_username', None)
+            password = request.form.get('_password', None)
+            # pylint: disable=unused-variable
+            realm = request.form.get('_realm', None)
+
+        # pylint: disable=broad-except
+        except Exception, errmsg:
+            if app.config.get('DEBUG', False):
+                import traceback
+                app.logger.error("Exception: %s" % (errmsg))
+                app.logger.error("%s" % (traceback.format_exc()))
+
+            return app.abort(403)
+
+        if username is None or password is None:
+            return app.abort(403)
+
+        account = db.session.query(Account).filter_by(
+            _name=username,
+            type_name='email'
+        ).first()
+
+        if account is None:
+            return app.abort(403)
+
+        if account.person is None:
+            return app.abort(403)
+
+        result = account.person.verify_password(password)
+
+        if result:
+            return "OK"
+        else:
+            return app.abort(403)
