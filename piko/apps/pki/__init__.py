dc7a0126532c2a28c0bf24d9c379dcee5b762038
Jeroen van Meeuwen <kanarip@kanarip.ch>
2017-01-15 19:47:58 +0100

    Clear style warnings

--- piko/apps/pki/__init__.py
+++ piko/apps/pki/__init__.py
@@ -1,3 +1,7 @@
+"""
+    piko.apps.pki
+    =============
+"""
 import os
 
 from flask import abort
@@ -5,15 +9,19 @@ from flask import Response
 
 from piko import App
 
-template_path = os.path.abspath(
-        os.path.join(
-                os.path.dirname(__file__),
-                'templates'
-            )
+TEMPLATE_PATH = os.path.abspath(
+    os.path.join(
+        os.path.dirname(__file__),
+        'templates'
     )
+)
+
 
 def register(apps):
-    app = App('piko.pki', template_folder = template_path)
+    """
+        Register the application
+    """
+    app = App('piko.pki', template_folder=TEMPLATE_PATH)
     app.debug = True
     register_routes(app)
 
@@ -21,26 +29,38 @@ def register(apps):
 
     return apps
 
+
 def register_blueprint(app):
+    """
+        Register a blueprint with the application
+    """
     from piko import Blueprint
 
     blueprint = Blueprint(
-            app,
-            'piko.pki',
-            __name__,
-            url_prefix='/pki',
-            template_folder=template_path
-        )
+        app,
+        'piko.pki',
+        __name__,
+        url_prefix='/pki',
+        template_folder=TEMPLATE_PATH
+    )
 
     register_routes(blueprint)
 
     app.register_blueprint(blueprint)
 
+
 def register_routes(app):
+    """
+        Register regular UI routes
+    """
     # TODO: Require authentication
     # TODO: Require ownership of the system, or admin role
     @app.route('/revoke/<system_uuid>')
+    # pylint: disable=unused-variable
     def request(system_uuid):
+        """
+            Request a certificate for a system.
+        """
         from piko.db import db
         from piko.apps.pki.db.model import Certificate
         from piko.apps.candlepin.db.model import System
@@ -61,7 +81,11 @@ def register_routes(app):
 
     # TODO: Require authentication
     @app.route('/download/<system_uuid>')
+    # pylint: disable=unused-variable
     def download(system_uuid):
+        """
+            Let a system download a certificate + key in PEM encoding
+        """
         from piko.db import db
         from piko.apps.pki.db.model import Certificate
         from piko.apps.candlepin.db.model import System
@@ -78,4 +102,7 @@ def register_routes(app):
             # TODO: Obscure the message
             return abort(404, "No such certificate")
 
-        return Response(cert.certificate + cert.private_key, mimetype='text/plain')
+        return Response(
+            cert.certificate + cert.private_key,
+            mimetype='text/plain'
+        )
