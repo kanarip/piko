7eb331473631e636970eb7540cae11f8233f9e67
Jeroen van Meeuwen <kanarip@kanarip.ch>
2017-01-15 19:50:04 +0100

    Clear style warnings

--- piko/apps/pki/ca.py
+++ piko/apps/pki/ca.py
@@ -1,46 +1,53 @@
+"""
+    The piko PKI CA routine of siging a certificate.
+"""
 import os
 
 from OpenSSL import crypto
 
+
 def sign_cert(cert):
+    """
+        Sign the certificate.
+    """
     base_path = os.path.join(
-            os.path.dirname(__file__),
-            '..', # up from pki/.
-            '..', # up from apps/.
-            '..', # up from piko/.
-            'tmp'
-        )
+        os.path.dirname(__file__),
+        '..',   # up from pki/.
+        '..',   # up from apps/.
+        '..',   # up from piko/.
+        'tmp'
+    )
 
     ca_path = os.path.join(base_path, 'ca')
 
     passphrase = open(
-            os.path.join(ca_path, 'mirror_ca.pass'),
-            'r'
-        ).read().strip()
+        os.path.join(ca_path, 'mirror_ca.pass'),
+        'r'
+    ).read().strip()
 
     ca_key = crypto.load_privatekey(
-            crypto.FILETYPE_PEM,
-            open(os.path.join(ca_path, 'mirror_ca.key'), 'r').read(),
-            passphrase = passphrase
-        )
+        crypto.FILETYPE_PEM,
+        open(os.path.join(ca_path, 'mirror_ca.key'), 'r').read(),
+        passphrase=passphrase
+    )
 
     ca_cert = crypto.load_certificate(
-            crypto.FILETYPE_PEM,
-            open(os.path.join(ca_path, 'mirror_ca.cert'), 'r').read()
-        )
+        crypto.FILETYPE_PEM,
+        open(os.path.join(ca_path, 'mirror_ca.cert'), 'r').read()
+    )
 
     ca_subject = ca_cert.get_subject()
     cert_issuer = cert.get_issuer()
 
     dn_attrs = [
-            'C',
-            'ST',
-            'L',
-            'O',
-            'OU',
-            'CN',
-            'emailAddress'
-        ]
+        'C',
+        'ST',
+        'L',
+        'O',
+        'OU',
+        'CN',
+        'emailAddress'
+    ]
 
     for dn_attr in dn_attrs:
         if hasattr(ca_subject, dn_attr):
@@ -48,41 +55,34 @@ def sign_cert(cert):
                 setattr(cert_issuer, dn_attr, getattr(ca_subject, dn_attr))
 
     cert.add_extensions(
-            [
-                    crypto.X509Extension(
-                            "subjectKeyIdentifier",
-                            False,
-                            "hash",
-                            subject = cert
-                        ),
+        [
+            crypto.X509Extension(
+                "subjectKeyIdentifier",
+                False,
+                "hash",
+                subject=cert
+            ),
 
-                    crypto.X509Extension(
-                            "basicConstraints",
-                            True,
-                            "CA:FALSE"
-                        ),
+            crypto.X509Extension(
+                "basicConstraints",
+                True,
+                "CA:FALSE"
+            ),
 
-                    #crypto.X509Extension(
-                    #        "keyUsage",
-                    #        False,
-                    #        "digitalSignature"
-                    #    ),
+            crypto.X509Extension(
+                "extendedKeyUsage",
+                False,
+                "clientAuth"
+            ),
 
-                    crypto.X509Extension(
-                            "extendedKeyUsage",
-                            False,
-                    #        "clientAuth,serverAuth"
-                            "clientAuth"
-                        ),
-
-                    crypto.X509Extension(
-                            "authorityKeyIdentifier",
-                            False,
-                            "keyid:always",
-                            issuer = ca_cert
-                        )
-                ]
-        )
+            crypto.X509Extension(
+                "authorityKeyIdentifier",
+                False,
+                "keyid:always",
+                issuer=ca_cert
+            )
+        ]
+    )
 
     cert.sign(ca_key, "md5")
     cert.sign(ca_key, "sha1")
