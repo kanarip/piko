b10eb25af2de7a0fcf3e5bc8249a37c683435df0
Jeroen van Meeuwen <kanarip@kanarip.ch>
2017-01-16 20:50:03 +0100

    Resolve pylint, pep8, flake8 style concerns
    Reference T6

--- piko/l10n.py
+++ piko/l10n.py
@@ -6,10 +6,13 @@ from flask import g
 from flask import request
 from flask import session
 
+# pylint: disable=no-name-in-module
+# pylint: disable=import-error
 from flask.ext.babel import Babel
 from flask.ext.babel import get_locale as get_babel_locale
 from flask.ext.babel import get_timezone as get_babel_timezone
 from flask.ext.babel import gettext
+
 
 def register_l10n(app):
     """
@@ -21,14 +24,15 @@ def register_l10n(app):
     from piko.cache import cache
 
     @babel.localeselector
+    # pylint: disable=unused-variable
     def get_locale():
-        locale = getattr(g, 'locale', None)
-
-        #if locale is not None:
-        #    return locale
-
+        """
+            Let babel select the correct locale.
+        """
         try:
             translations = get_translations()
+
+        # pylint: disable=unused-variable
         except ValueError, errmsg:
             try:
                 cache.delete_memoized(get_translations)
@@ -39,7 +43,7 @@ def register_l10n(app):
 
         result = request.accept_languages.best_match(translations)
 
-        if result == None:
+        if result is None:
             result = 'en'
 
         g.locale = result
@@ -49,10 +53,14 @@ def register_l10n(app):
         return g.get('locale')
 
     @babel.timezoneselector
+    # pylint: disable=unused-variable
     def get_timezone():
+        """
+            Let babel use the correct timezone.
+        """
         timezone = getattr(g, 'timezone', None)
 
-        if not timezone == None:
+        if timezone is not None:
             return timezone
 
         g.timezone = "UTC"
@@ -61,12 +69,16 @@ def register_l10n(app):
 
     @cache.memoize()
     def get_translations():
+        """
+            Get a list of translations in a form we understand.
+        """
         translations = [x.language for x in babel.list_translations()]
         return translations
 
+
 __all__ = [
-        'get_babel_locale',
-        'get_babel_timezone',
-        'gettext',
-        'register_l10n'
-    ]
+    'get_babel_locale',
+    'get_babel_timezone',
+    'gettext',
+    'register_l10n'
+]
