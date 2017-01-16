ee3de7541f8e2a889016b53d072cb939a4be120b
Jeroen van Meeuwen <kanarip@kanarip.ch>
2017-01-16 20:36:42 +0100

    Resolve pylint, pep8, flake8 style concerns
    Reference T6

--- piko/forms/login/email.py
+++ piko/forms/login/email.py
@@ -1,3 +1,13 @@
+"""
+    piko.forms.LoginEmailForm
+    =========================
+
+    Provides a CSRF-protected login form for use with an email address and
+    password.
+"""
+
+# pylint: disable=no-name-in-module
+# pylint: disable=import-error
 from flask.ext.wtf import Form
 
 from wtforms import HiddenField
@@ -6,8 +16,22 @@ from wtforms import PasswordField
 from wtforms import SubmitField
 from wtforms import validators
 
+
+# pylint: disable=too-few-public-methods
 class LoginEmailForm(Form):
+    """
+        A :py:class:`Form` definition.
+    """
     uuid = HiddenField('UUID')
-    email_address = TextField('Email Address', validators=[validators.Required(), validators.Email()])
+
+    email_address = TextField(
+        'Email Address',
+        validators=[
+            validators.Required(),
+            validators.Email()
+        ]
+    )
+
     password = PasswordField('Password', validators=[validators.Required()])
+
     submit = SubmitField('Login')
